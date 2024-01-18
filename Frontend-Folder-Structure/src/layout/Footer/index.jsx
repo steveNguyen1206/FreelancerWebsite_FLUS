6f190644947ff56368904f19affde6ecd1df5cee
hdkhang6803 <hdkhang6803@gmail.com>
2024-01-18 16:35:31 +0700

    fix footer

--- Frontend-Folder-Structure/src/layout/Footer/index.jsx
+++ Frontend-Folder-Structure/src/layout/Footer/index.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
               </span>
               <span className={styles.span}>
                 <br />
-                aaaaaaaaaaaaaa
+                Discover a world of elite freelancers, streamline collaboration, and witness unparalleled success. Elevate your vision, connect effortlessly, and experience the future of freelance excellence. FLUS - where projects meet perfection!
               </span>
             </p>
           </div>
